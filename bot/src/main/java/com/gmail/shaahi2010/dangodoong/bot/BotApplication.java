2fc6a9e1c57a96bf9e187ddf60e119f35a6bce95
Shaahin Yazarloo <shaahin2010@gmail.com>
2020-09-13 01:28:37 +0430

    init bot project

---
+++ bot/src/main/java/com/gmail/shaahi2010/dangodoong/bot/BotApplication.java
@@ -0,0 +1,13 @@
+package com.gmail.shaahi2010.dangodoong.bot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BotApplication.class, args);
+	}
+
+}
---
+++ bot/src/test/java/com/gmail/shaahi2010/dangodoong/bot/BotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gmail.shaahi2010.dangodoong.bot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
